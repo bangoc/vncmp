f6e0e2c0865d9bc0017985e04026bf31acdaef14
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2023-02-03 08:24:10 +0700

    Giản lược các hàm so sánh chuỗi

--- vncmp.c
+++ vncmp.c
@@ -16,35 +16,18 @@ int vnwchcmp(wchar_t ch1, wchar_t ch2) {
 }
 
 int vnwscmp(const wchar_t *s1, const wchar_t *s2) {
-  wchar_t ch1, ch2;
-  for (;;) {
-    ch1 = *s1;
-    ch2 = *s2;
-    if (ch1 == 0 || ch2 == 0) {
-      break;
-    }
-    if (ch1 == ch2) {
-      ++s1;
-      ++s2;
-      continue;
-    }
-    break;
+  while (*s1 == *s2 && *s1 != 0) {
+    ++s1;
+    ++s2;
   }
-  return VNCHMAP(ch1) - VNCHMAP(ch2);
+  return VNCHMAP(*s1) - VNCHMAP(*s2);
 }
 
 int vnu8scmp(const char *s1, const char *s2) {
   wchar_t ch1, ch2;
-  for (;;) {
+  do {
     s1 += u82wc(&ch1, s1);
     s2 += u82wc(&ch2, s2);
-    if (ch1 == 0 || ch2 == 0) {
-      break;
-    }
-    if (ch1 == ch2) {
-      continue;
-    }
-    break;
-  }
+  } while (ch1 == ch2 && ch1 != 0);
   return VNCHMAP(ch1) - VNCHMAP(ch2);
 }
